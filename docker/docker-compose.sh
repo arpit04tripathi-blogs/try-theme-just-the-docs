cf8e0d566432d12d9836e07efc57e5eb0855be4a
Arpit Tripathi <arpit04tripathi@gmail.com>
2025-06-20 18:11:18 +0100

    docker

--- docker/docker-compose.sh
+++ docker/docker-compose.sh
@@ -7,13 +7,5 @@ echo =========================================================
 # echo "<=== info minima ===>"
 # bundle info minima
 
-if [[ $# -gt 0 ]] && [[ $1 = '--new' ]]
-then
-  echo "<=== Creating new Jekyll project ===>"
-  jekyll new ./new_site
-  mv new_site/* .
-  rm -r new_site
-else
-  echo "<=== serve incremental ===>"
-  jekyll serve --incremental
-fi
+echo "<=== serve incremental ===>"
+jekyll serve --incremental
